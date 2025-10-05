0bfcbd53b7360bc6f0b8cd9cf17e828c3de8d452
dasbred014-cloud <dasbred014@gmail.com>
2025-10-05 13:52:28 +0200

    Aggiunta cartella Files

---
+++ Files/core/resources/views/templates/sunfyre/layouts/master.blade.php
@@ -0,0 +1,26 @@
+@extends($activeTemplate . 'layouts.app')
+@section('app')
+    @include($activeTemplate . 'partials.user_header')
+
+    @if (!request()->routeIs('home'))
+        @include($activeTemplate . 'partials.breadcrumb')
+    @endif
+
+    <section class="py-100">
+        <div class="container">
+            @yield('content')
+        </div>
+    </section>
+
+
+    @include($activeTemplate . 'partials.footer')
+@endsection
+
+@push('script')
+    <script>
+        "use strict";
+        $(document).on('click touchstart', function(e) {
+            $('.win-loss-popup').removeClass('active');
+        });
+    </script>
+@endpush
